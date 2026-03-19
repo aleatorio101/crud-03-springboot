10f64b8a32b73d6d2ee4fa91e5a4a7e915ed09b7
aleatorio101 <lukasdng@gmail.com>
2026-03-19 17:39:43 -0300

    criacao de repository

---
+++ src/main/java/com/example/repository/PedidoRepository.java
@@ -0,0 +1,8 @@
+
+package com.example.repository;
+
+import com.example.entity.Pedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PedidoRepository extends JpaRepository<Pedido, Long> {
+}
